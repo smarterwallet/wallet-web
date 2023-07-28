4b8f46090678937861f0bc8594e1a8c8afa4f024
Gaollg <gaollg@qq.com>
2023-07-28 23:53:28 +0800

    fix: 增加 @ethersproject/bignumber，解决报错

---
+++ src/app/pages/register/RegisterAccountTypePage.tsx
@@ -0,0 +1,17 @@
+import SWCell from "../../component/SWCell"
+import HeaderBar from "../../elements/HeaderBar"
+
+export default function RegisterAccountTypePage(props: {}) {
+  return (
+    <div className="register-page">
+      <HeaderBar text="Account Signup" />
+      <div>Choose the type of account you want to have:</div>
+
+      <div className="register-page-title">Choose a way to register your account:</div>
+
+      <SWCell icon="/icon/lock.png" text="Password" to="/registerPwd" shadow={true} />
+
+      <SWCell icon="/icon/lock.png" text="Multi party account" to="/registerPwd" shadow={true} />
+    </div>
+  )
+}
