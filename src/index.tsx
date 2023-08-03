846e7fb09d464b217091a618f33e46ec7e324aaa
vivomo <929992114@qq.com>
2023-08-03 11:25:48 +0800

    fix: 样式整理

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
+import './styles/common.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
