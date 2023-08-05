7de275df6195ccc58779f8a466f805cda6098600
Gaollg <gaollg@qq.com>
2023-08-06 00:17:57 +0800

    feat: add MultiPartyQuantityChoosePage

--- src/app/pages/signin/multi/MultiPartyQuantityChoosePage.tsx
+++ src/app/pages/signin/multi/MultiPartyQuantityChoosePage.tsx
@@ -1,20 +1,18 @@
 import { NavLink } from 'react-router-dom';
 import React from 'react';
 import HeaderBar from '../../../elements/HeaderBar';
+import SWCell from '../../../component/SWCell';
 
 export default function MultiPartyQuantityChoosePage(props: {}) {
   return (
     <div className="px-[20px]">
       <HeaderBar text="Multi-party Backup" />
 
-      <div className="mt-16">How many login keys would you like to set?</div>
-      <div className="">
-        <NavLink className="welcome-page-button-container" to="/signin/singlePartyAccount">
-          <div className="welcome-page-button-text">3</div>
-          <img className="welcome-page-button-icon" src="/icon/arrow-right.png" />
-        </NavLink>
+      <div className="text-2xl">How many login keys would you like to set?</div>
+      <div className="mt-4">
+        <SWCell text="3" to="/signin/multiPartyQuantityChoosePage" shadow={true} />
       </div>
-      <div className="px-4">At least 2 of 3 keys will be required to login smoothly</div>
+      <div className="mt-4 px-4">At least 2 of 3 keys will be required to login smoothly</div>
     </div>
   );
 }
