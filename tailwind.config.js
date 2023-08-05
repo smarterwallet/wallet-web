82d2532b6883c59e15fff85bbb0de740f08a1f83
Gaollg <gaollg@qq.com>
2023-08-05 19:12:05 +0800

    fix: 修复 tailwind 与 antd mobild 冲突

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,7 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  corePlugins: {
+    preflight: false, // 解决 button `background-color: transparent;` 问题 https://www.jianshu.com/p/2162daf865d9。
+  },
 };
