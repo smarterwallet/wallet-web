20fc12dc25215d31c959dba2d466c0a16474da7d
vivomo <929992114@qq.com>
2023-08-07 00:22:30 +0800

    fix: 样式优化

--- src/app/pages/SpotGridBot/index.tsx
+++ src/app/pages/SpotGridBot/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { Select, Form, InputNumber, Button, Space, Modal } from 'antd';
-import HeaderBar from '../elements/HeaderBar';
+import HeaderBar from '../../elements/HeaderBar';
 import { useNavigate } from 'react-router-dom';
 
-const SpotGridBot = () => {
+const Index = () => {
   const navigate = useNavigate();
+
+  const run = () => {
+    Modal.success({
+      closable: true,
+      title: 'Run successfully!',
+      content: 'AA bundlers will run your trading bot to execute your trading strategy, and submit corresponding transactions to trade for you under your authorization',
+      footer: (
+        <Space style={{ width: '100%', justifyContent: 'end', marginTop: 20 }}>
+          <Button>View</Button>
+          <Button>Done</Button>
+        </Space>
+      )
+    })
+  }
 
   return (
     <div className="ww-page-container">
@@ -13,7 +27,7 @@ const SpotGridBot = () => {
         <h3>Trading asset</h3>
         <Space>
           <Form.Item>
-            <Select defaultValue="Type">
+            <Select defaultValue="Type" className="ww-selector">
               <Select.Option value="Type">Type</Select.Option>
             </Select>
           </Form.Item>
@@ -24,7 +38,7 @@ const SpotGridBot = () => {
         <h3>Starting condition</h3>
         <Space>
           <Form.Item>
-            <Select defaultValue="Price">
+            <Select defaultValue="Price" className="ww-selector">
               <Select.Option value="Price">Price</Select.Option>
             </Select>
           </Form.Item>
@@ -35,7 +49,7 @@ const SpotGridBot = () => {
         <h3>Ending condition</h3>
         <Space>
           <Form.Item>
-            <Select defaultValue="Price">
+            <Select defaultValue="Price" className="ww-selector">
               <Select.Option value="Price">Price</Select.Option>
             </Select>
           </Form.Item>
@@ -46,7 +60,7 @@ const SpotGridBot = () => {
         <h3>Gas asset</h3>
         <Space>
           <Form.Item>
-            <Select defaultValue="Type">
+            <Select defaultValue="Type" className="ww-selector">
               <Select.Option value="Type">Type</Select.Option>
             </Select>
           </Form.Item>
@@ -54,24 +68,12 @@ const SpotGridBot = () => {
             <InputNumber placeholder="Amount"/>
           </Form.Item>
         </Space>
-        <Form.Item>
-          <Button onClick={() => {
-            Modal.success({
-              closable: true,
-              title: 'Run successfully!',
-              content: 'AA bundlers will run your trading bot to execute your trading strategy, and submit corresponding transactions to trade for you under your authorization',
-              footer: (
-                <Space style={{ width: '100%', justifyContent: 'end', marginTop: 20 }}>
-                  <Button>View</Button>
-                  <Button>Done</Button>
-                </Space>
-              )
-            })
-          }}>Run</Button>
-        </Form.Item>
+        <div className="ww-tc">
+          <Button type="primary" shape="round" onClick={run}>Run</Button>
+        </div>
       </Form>
     </div>
   );
 };
 
-export default SpotGridBot;
+export default Index;
