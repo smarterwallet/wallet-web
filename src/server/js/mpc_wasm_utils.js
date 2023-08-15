ae42ef211037eddf1003db0d3ea0dd79937e9378
Myron <>
2023-08-15 11:09:19 +0800

    fix: build error

--- src/server/js/mpc_wasm_utils.js
+++ src/server/js/mpc_wasm_utils.js
@@ -1,22 +1,19 @@
 const JSONBig = require('json-bigint');
 let runWasmModule;
 
-module.exports = {
+export const JSONBigInt = JSONBig({
+    storeAsString: true, // 将大整数存储为字符串
+    strict: true, // 启用严格模式，禁用科学计数法
+})
 
-    JSONBigInt: JSONBig({
-        storeAsString: true, // 将大整数存储为字符串
-        strict: true, // 启用严格模式，禁用科学计数法
-    }),
-
-    init: async function(buffer){
-        if (!runWasmModule) {
-            runWasmModule = await require('./run_wasm')(buffer);
-        }
-    },
-
-    generateDeviceData: async function () {
-        console.log("start run generateDeviceData...")
-        return generateDeviceData();
+export async function initWasm(buffer) {
+    if (!runWasmModule) {
+        runWasmModule = await require('./run_wasm')(buffer);
     }
+}
 
-};
+export async function generateDeviceData() {
+    console.log("start run generateDeviceData...")
+    return generateDeviceData();
+}
+
