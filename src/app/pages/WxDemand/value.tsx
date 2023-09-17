6efa1e29e7b7f706f60aa868e4edc383ad7547a6
Myron <>
2023-09-17 10:16:07 +0800

    update

--- src/app/pages/WxDemand/value.tsx
+++ src/app/pages/WxDemand/value.tsx
@@ -4,8 +4,8 @@ const Value = () => {
   return (
     <div className="value-content">
       <h2>Hotdog道具自动交易方案推荐</h2>
-      <p className="main-txt"><strong>游戏道具：</strong>棉花    <strong>数量：</strong>10个 （50%）</p>
-      <p className="main-txt"><strong>当前兑换比例：</strong>1棉花 = 5棉团 （-5%）</p>
+      <p className="main-txt"><strong>游戏道具：</strong>潮牌服饰    <strong>数量：</strong>10个 （50%）</p>
+      <p className="main-txt"><strong>当前兑换比例：</strong>1 潮牌服饰 = 5 Hotdog （-5%）</p>
       <p className="main-txt"><strong>交易策略：</strong>游戏进攻+公会打金+市场交易</p>
       <p className="main-txt"> <strong>具体执行步骤：</strong></p>
       <div className="step-txt">1.自动搜寻并开启敌军武力值&lt;80%，健康值&lt;60%的游戏，主动发起进攻</div>
