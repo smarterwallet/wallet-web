7a0afa857bcb13f3bde93bd5303430415ac20de7
gray_jiang <Gray_Jy@yeah.net>
2023-12-04 16:07:33 +0800

    ✨ feat(Cross): cross-chain basic ui

--- src/server/account/MPCManageAccount.ts
+++ src/server/account/MPCManageAccount.ts
@@ -1,23 +1,22 @@
-import { ethers } from "ethers";
+import { ethers } from 'ethers';
 import yuxStorage from '../utils/DBUtils';
-import { AccountInterface } from "./AccountInterface";
+import { AccountInterface } from './AccountInterface';
 import * as mpcWasmUtils from '../js/mpc_wasm_utils.js';
-import { JSONBigInt } from "../js/common_utils";
-import { Config } from "../config/Config";
-import { HttpUtils } from "../utils/HttpUtils";
-import { ERC4337BaseManageAccount } from "./ERC4337BaseManageAccount";
-import { hashMessage, joinSignature } from "ethers/lib/utils";
-import { CryptologyUtils } from "../utils/CryptologyUtils";
-import { Global } from "../Global";
+import { JSONBigInt } from '../js/common_utils';
+import { Config } from '../config/Config';
+import { HttpUtils } from '../utils/HttpUtils';
+import { ERC4337BaseManageAccount } from './ERC4337BaseManageAccount';
+import { hashMessage, joinSignature } from 'ethers/lib/utils';
+import { CryptologyUtils } from '../utils/CryptologyUtils';
+import { Global } from '../Global';
 import { ab2str, str2ab } from '../utils/TxUtils';
 
-const { arrayify } = require("@ethersproject/bytes");
+const { arrayify } = require('@ethersproject/bytes');
 
 /**
  * MPC Account Manage
  */
 export class MPCManageAccount extends ERC4337BaseManageAccount implements AccountInterface {
-
   /**
    * key for sign and get MPC address
    */
@@ -65,7 +64,7 @@ export class MPCManageAccount extends ERC4337BaseManageAccount implements Accoun
       return;
     }
 
-    console.log("mpc key:", mpcKey);
+    console.log('mpc key:', mpcKey);
     await super.initAccount(mpcKey);
 
     this._mpcKey = mpcKey;
@@ -73,77 +72,85 @@ export class MPCManageAccount extends ERC4337BaseManageAccount implements Accoun
     this.ethersProvider = new ethers.providers.JsonRpcProvider(Config.RPC_API);
     let account = ethers.Wallet.createRandom();
     this.ethersWallet = new ethers.Wallet(account.privateKey, this.ethersProvider);
-    if (mpcKey !== "") {
-      console.log("mpc not null");
+    if (mpcKey !== '') {
+      console.log('mpc not null');
       const initP1KeyDataRes = await mpcWasmUtils.wasmInitP1KeyData(mpcKey);
-      console.log("initP1KeyData: ", initP1KeyDataRes);
+      console.log('initP1KeyData: ', initP1KeyDataRes);
       this.contractWalletAddress = await this.calcContractWalletAddress();
       this.deployContractWalletIfNotExist(await this.getOwnerAddress());
     } else {
-      console.log("mpc is null")
+      console.log('mpc is null');
       this.contractWalletAddress = null;
     }
     this.contractAddressExist = false;
   }
 
   async getOwnerAddress(): Promise<string> {
-    if (Global.authorization == null || Global.authorization === "") {
-      console.log("have not login wallet server");
+    if (Global.authorization == null || Global.authorization === '') {
+      console.log('have not login wallet server');
       return null;
     }
-    if (this.mpcAddress != null && this.mpcAddress !== "") {
+    if (this.mpcAddress != null && this.mpcAddress !== '') {
       return this.mpcAddress;
     }
     // get address
     // params: p1 key, p2 id, random prim1, random prim2
-    console.log("start to get address")
+    console.log('start to get address');
 
-    console.log("start to get random prim(each client only needs to get it once)")
+    console.log('start to get random prim(each client only needs to get it once)');
     let primResult;
     // 从 localStorage 获取数据
-    const primKey = "primResult";
+    const primKey = 'primResult';
     let data = localStorage.getItem(primKey);
-    if (data != null && data !== "") {
-      console.log("read prim from local storage");
+    if (data != null && data !== '') {
+      console.log('read prim from local storage');
       primResult = JSON.parse(data);
     } else {
-      let primRequestResult = await HttpUtils.get(Config.BACKEND_API + "/mpc/calc/get-prim");
-      if (primRequestResult.body["code"] != 200) {
-        throw new Error(primRequestResult.body["message"]);
+      let primRequestResult = await HttpUtils.get(Config.BACKEND_API + '/mpc/calc/get-prim');
+      if (primRequestResult.body['code'] !== 200) {
+        throw new Error(primRequestResult.body['message']);
       }
-      primResult = primRequestResult.body["result"];
+      primResult = primRequestResult.body['result'];
       localStorage.setItem(primKey, JSON.stringify(primResult));
     }
     // console.log("primResult:", primResult);
-    const prim1 = primResult["p"];
-    const prim2 = primResult["q"];
+    const prim1 = primResult['p'];
+    const prim2 = primResult['q'];
 
     const addressGenMessage = await mpcWasmUtils.wasmKeyGenRequestMessage(2, prim1, prim2);
     // console.log("Generate address Request Message: ", addressGenMessage);
     let addressGenMessageJson = JSONBigInt.parse(addressGenMessage);
-    console.log(addressGenMessageJson["data"]);
+    console.log(addressGenMessageJson['data']);
 
-    console.log("start to bind-user-p2")
-    let bindResult = await HttpUtils.postWithAuth(Config.BACKEND_API + "/mpc/calc/bind-user-p2", {
-      "p1_message_dto": addressGenMessageJson["data"],
-      "p1_data_id": 1,
-    }, Global.authorization);
-    if (bindResult.body["code"] != 200) {
-      throw new Error(bindResult.body["message"]);
+    console.log('start to bind-user-p2');
+    let bindResult = await HttpUtils.postWithAuth(
+      Config.BACKEND_API + '/mpc/calc/bind-user-p2',
+      {
+        p1_message_dto: addressGenMessageJson['data'],
+        p1_data_id: 1,
+      },
+      Global.authorization,
+    );
+    if (bindResult.body['code'] !== 200) {
+      throw new Error(bindResult.body['message']);
     }
-    console.log("bindResult:", bindResult.body);
+    console.log('bindResult:', bindResult.body);
 
     // send http request to get address
-    console.log("start to get address")
-    let getAddressAndPubKeyRes = await HttpUtils.postWithAuth(Config.BACKEND_API + "/mpc/calc/get-address", {}, Global.authorization)
-    if (getAddressAndPubKeyRes.body["code"] != 200) {
-      throw new Error(getAddressAndPubKeyRes.body["message"]);
+    console.log('start to get address');
+    let getAddressAndPubKeyRes = await HttpUtils.postWithAuth(
+      Config.BACKEND_API + '/mpc/calc/get-address',
+      {},
+      Global.authorization,
+    );
+    if (getAddressAndPubKeyRes.body['code'] !== 200) {
+      throw new Error(getAddressAndPubKeyRes.body['message']);
     }
-    const address = getAddressAndPubKeyRes.body["result"]["address"];
-    const pubKey = getAddressAndPubKeyRes.body["result"]["pub_key"];
-    console.log("Address: " + address);
-    console.log("PubKey: " + pubKey);
-    const initPubKeyRes = mpcWasmUtils.wasmInitPubKey(pubKey);
+    const address = getAddressAndPubKeyRes.body['result']['address'];
+    const pubKey = getAddressAndPubKeyRes.body['result']['pub_key'];
+    console.log('Address: ' + address);
+    console.log('PubKey: ' + pubKey);
+    mpcWasmUtils.wasmInitPubKey(pubKey);
     // console.log(`initPubKey: ${initPubKeyRes}`);
     this.mpcAddress = address;
     return address;
@@ -153,13 +160,17 @@ export class MPCManageAccount extends ERC4337BaseManageAccount implements Accoun
     let hash = hashMessage(arrayify(message));
     hash = hash.substring(2);
     // send http request to get address
-    console.log("start to init-p2-content")
-    let initP2ContentRes = await HttpUtils.postWithAuth(Config.BACKEND_API + "/mpc/calc/init-p2-content", {
-      "message": hash
-    }, Global.authorization)
-    console.log("initP2ContentRes: ", initP2ContentRes);
-    if (initP2ContentRes.body["code"] != 200) {
-      throw new Error(initP2ContentRes.body["message"]);
+    console.log('start to init-p2-content');
+    let initP2ContentRes = await HttpUtils.postWithAuth(
+      Config.BACKEND_API + '/mpc/calc/init-p2-content',
+      {
+        message: hash,
+      },
+      Global.authorization,
+    );
+    console.log('initP2ContentRes: ', initP2ContentRes);
+    if (initP2ContentRes.body['code'] !== 200) {
+      throw new Error(initP2ContentRes.body['message']);
     }
     // Step 0
     // params: p1 key, p2 id, random prim1, random prim2
@@ -171,57 +182,68 @@ export class MPCManageAccount extends ERC4337BaseManageAccount implements Accoun
     console.log(`p1Step1: ${p1Step1Res}`);
 
     // p2 step1
-    let p2Step1Result = await HttpUtils.postWithAuth(Config.BACKEND_API + "/mpc/calc/p2-step1", {
-      "commitment": JSONBigInt.parse(p1Step1Res)["data"],
-    }, Global.authorization)
-    console.log("p2Step1Result: ", p2Step1Result);
-    if (p2Step1Result.body["code"] != 200) {
-      throw new Error(p2Step1Result.body["message"]);
+    let p2Step1Result = await HttpUtils.postWithAuth(
+      Config.BACKEND_API + '/mpc/calc/p2-step1',
+      {
+        commitment: JSONBigInt.parse(p1Step1Res)['data'],
+      },
+      Global.authorization,
+    );
+    console.log('p2Step1Result: ', p2Step1Result);
+    if (p2Step1Result.body['code'] !== 200) {
+      throw new Error(p2Step1Result.body['message']);
     }
 
-    let proofJson = p2Step1Result.body["result"]["proof"]
-    console.log("p2Step1Result proofJson: ", proofJson);
-    proofJson = mpcWasmUtils.parseNumbers(proofJson)
-    console.log("p2Step1Result proofJsonStr: ", JSONBigInt.stringify(proofJson));
+    let proofJson = p2Step1Result.body['result']['proof'];
+    console.log('p2Step1Result proofJson: ', proofJson);
+    proofJson = mpcWasmUtils.parseNumbers(proofJson);
+    console.log('p2Step1Result proofJsonStr: ', JSONBigInt.stringify(proofJson));
 
-    let ecpointJson = p2Step1Result.body["result"]["ecpoint"]
-    ecpointJson = mpcWasmUtils.parseNumbers(ecpointJson)
-    console.log("p2Step1Result ecpointJsonStr: ", JSONBigInt.stringify(ecpointJson));
+    let ecpointJson = p2Step1Result.body['result']['ecpoint'];
+    ecpointJson = mpcWasmUtils.parseNumbers(ecpointJson);
+    console.log('p2Step1Result ecpointJsonStr: ', JSONBigInt.stringify(ecpointJson));
 
     // p1 step2
-    const p1Step2Res = await mpcWasmUtils.wasmP1Step2(JSONBigInt.stringify(proofJson), JSONBigInt.stringify(ecpointJson));
+    const p1Step2Res = await mpcWasmUtils.wasmP1Step2(
+      JSONBigInt.stringify(proofJson),
+      JSONBigInt.stringify(ecpointJson),
+    );
     console.log(`p1Step2: ${p1Step2Res}`);
 
-    const p1Step2ResJSON = JSONBigInt.parse(p1Step2Res)
-    let p1ProofJson = p1Step2ResJSON["data"]["SchnorrProofOutput"]
-    p1ProofJson = mpcWasmUtils.parseNumbers(p1ProofJson)
-    console.log("p1Step2Res p1ProofJson: ", JSONBigInt.stringify(p1ProofJson));
+    const p1Step2ResJSON = JSONBigInt.parse(p1Step2Res);
+    let p1ProofJson = p1Step2ResJSON['data']['SchnorrProofOutput'];
+    p1ProofJson = mpcWasmUtils.parseNumbers(p1ProofJson);
+    console.log('p1Step2Res p1ProofJson: ', JSONBigInt.stringify(p1ProofJson));
 
-    let cmtDJson = p1Step2ResJSON["data"]["Witness"]
-    cmtDJson = mpcWasmUtils.parseNumbers(cmtDJson)
-    console.log("p1Step2Res cmtDJson: ", JSONBigInt.stringify(cmtDJson));
+    let cmtDJson = p1Step2ResJSON['data']['Witness'];
+    cmtDJson = mpcWasmUtils.parseNumbers(cmtDJson);
+    console.log('p1Step2Res cmtDJson: ', JSONBigInt.stringify(cmtDJson));
 
     // p2 step2
-    let p2Step2Result = await HttpUtils.postWithAuth(Config.BACKEND_API + "/mpc/calc/p2-step2", {
-      "cmt_d": cmtDJson,
-      "p1_proof": p1ProofJson,
-    }, Global.authorization)
-    console.log("p2Step2Result: ", p2Step2Result);
-    if (p2Step2Result.body["code"] != 200) {
-      throw new Error(p2Step2Result.body["message"]);
+    let p2Step2Result = await HttpUtils.postWithAuth(
+      Config.BACKEND_API + '/mpc/calc/p2-step2',
+      {
+        cmt_d: cmtDJson,
+        p1_proof: p1ProofJson,
+      },
+      Global.authorization,
+    );
+    console.log('p2Step2Result: ', p2Step2Result);
+    if (p2Step2Result.body['code'] !== 200) {
+      throw new Error(p2Step2Result.body['message']);
     }
 
     // p1 step3
-    const p1Step3Res = await mpcWasmUtils.wasmP1Step3(p2Step2Result.body["result"], hash);
+    const p1Step3Res = await mpcWasmUtils.wasmP1Step3(p2Step2Result.body['result'], hash);
     console.log(`p1Step2: ${p1Step3Res}`);
 
-    const signHex = "0x" + JSONBigInt.parse(p1Step3Res)["data"]["SignHex"];
+    const signHex = '0x' + JSONBigInt.parse(p1Step3Res)['data']['SignHex'];
     const signForContract = joinSignature(signHex);
     return signForContract;
   }
 
   private async generateMPCWasmInstance() {
-    console.log("generateMPCWasmInstance start");
+    console.log('generateMPCWasmInstance start');
     const response = await fetch(Config.MPC_WASM_URL);
     let buffer = await response.arrayBuffer();
     // let buffer = null;
@@ -248,23 +270,27 @@ export class MPCManageAccount extends ERC4337BaseManageAccount implements Accoun
   public async generateKeys() {
     const keysResult = await mpcWasmUtils.wasmGenerateDeviceData();
     const keysJson = JSONBigInt.parse(keysResult);
-    if (keysJson["code"] === 200) {
-      console.log("p1JsonData: " + JSONBigInt.stringify(keysJson["data"]["p1JsonData"]));
-      console.log("p2JsonData: " + JSONBigInt.stringify(keysJson["data"]["p2JsonData"]));
-      console.log("p3JsonData: " + JSONBigInt.stringify(keysJson["data"]["p3JsonData"]));
-      return keysJson["data"];
+    if (keysJson['code'] === 200) {
+      console.log('p1JsonData: ' + JSONBigInt.stringify(keysJson['data']['p1JsonData']));
+      console.log('p2JsonData: ' + JSONBigInt.stringify(keysJson['data']['p2JsonData']));
+      console.log('p3JsonData: ' + JSONBigInt.stringify(keysJson['data']['p3JsonData']));
+      return keysJson['data'];
     } else {
-      console.log("generateDeviceData error. Response: " + keysResult);
+      console.log('generateDeviceData error. Response: ' + keysResult);
       return null;
     }
   }
 
   async saveKey2WalletServer(key: string) {
     let api = Config.BACKEND_API + '/mpc/key/save';
-    console.log("this._authorization:", Global.authorization)
-    return HttpUtils.postWithAuth(api, {
-      "key": key
-    }, Global.authorization);
+    console.log('this._authorization:', Global.authorization);
+    return HttpUtils.postWithAuth(
+      api,
+      {
+        key: key,
+      },
+      Global.authorization,
+    );
   }
 
   saveKey2LocalStorage(key: string, password: string): boolean {
@@ -289,18 +315,21 @@ export class MPCManageAccount extends ERC4337BaseManageAccount implements Accoun
     key = CryptologyUtils.encrypt(key, password);
     let api = Config.DECENTRALIZE_STORAGE_API + '/ipfs/upload';
     return HttpUtils.post(api, {
-      "data": key
+      data: key,
     });
   }
 
   existLocalStorageKey(): boolean {
-    return localStorage.getItem(Config.LOCAL_STORAGE_MPC_KEY1) != null && localStorage.getItem(Config.LOCAL_STORAGE_MPC_KEY1).length != 0;
+    return (
+      localStorage.getItem(Config.LOCAL_STORAGE_MPC_KEY1) !== null &&
+      localStorage.getItem(Config.LOCAL_STORAGE_MPC_KEY1).length !== 0
+    );
   }
 
   getKeyFromLocalStorage(password: string): string {
     const keyInLocal = localStorage.getItem(Config.LOCAL_STORAGE_MPC_KEY1);
     const data = CryptologyUtils.decrypt(keyInLocal, password);
-    if (data == null || data === "") {
+    if (data == null || data === '') {
       return data;
     }
     return mpcWasmUtils.parseNumbers(JSONBigInt.parse(data));
@@ -315,6 +344,4 @@ export class MPCManageAccount extends ERC4337BaseManageAccount implements Accoun
   updateLocalKey(password: string): boolean {
     return this.saveKey2LocalStorage(this.key, password);
   }
-
 }
-
