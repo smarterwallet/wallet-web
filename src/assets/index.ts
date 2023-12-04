285cdd27078bde5bba765898e8a55110074b1e15
gray_jiang <Gray_Jy@yeah.net>
2023-12-04 23:47:36 +0800

    ✨ feat(ConfirmModal): two status modal

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+import Bg from './bg.png';
+import chainlinkIcon from './chainlink.png';
+import transferIcon from './transfer.png';
+import downArrow from './down-arrow.png';
+
+export { Bg, chainlinkIcon, transferIcon, downArrow };
