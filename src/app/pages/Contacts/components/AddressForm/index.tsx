9f44ec8cb0716565c6429915d931d42c3366343c
Ruiz000 <943461216@qq.com>
2023-12-05 17:02:08 +0800

    🦄 refactor(AddressForm.tsx,SendForm.tsx,ReceiptForm.tsx): 重构组件结构，实现了存储本地联系人的功能
    减少了对Form组件的使用，样式相对更好控制，但失去了Form提供的数值检查与错误提示交互，在添加联系人函数中进行了一系列条件检查。

--- src/app/pages/Contacts/components/AddressForm/index.tsx
+++ src/app/pages/Contacts/components/AddressForm/index.tsx
@@ -1,42 +1,52 @@
-import { Form, Input, Picker, Button } from 'antd-mobile';
+import { Form, Input, Picker, Button, Card } from 'antd-mobile';
 import { useState } from 'react';
 import { TransactionDetail } from '../../index';
+import './styles.scss';
 
 type Props = TransactionDetail & {
   onChange: (key: keyof TransactionDetail, value: any) => void;
-};
-const AddressForm: React.FC<Props> = ({ source, target, address, receiver, amount, token, onChange }) => {
+}
+const AddressForm: React.FC<Props> = ({ source, target, address, receiver, amount, token, onChange, }) => {
   const [visible, setVisible] = useState(false);
   const [blockchain, setBlockChain] = useState<(string | null)[]>(['']); // 會被props傳進來 這裏做交互測試
-  const blockchainColumns = [[{ label: 'Mumbai', value: 'mumbai' }], [{ label: 'Fuji', value: 'fuji' }]];
+  const blockchainColumns = [[{ label: 'Mumbai', value: 'mumbai' },{ label: 'Fuji', value: 'fuji' } ]];
 
   return (
-    <div>
-      <Form>
-        <Form.Header>Address:</Form.Header>
-        <Form.Item>
-          <Input />
-        </Form.Item>
-
-        <Form.Header>Blockchain:</Form.Header>
-        <Form.Item>
-          <Button className="w-full border-none" onClick={() => setVisible(true)}>
-            选择网络
-          </Button>
+    <div className="bg-transparent h-[28rem] flex flex-col">
+      {/* address */}
+      <div className="grow flex flex-col px-16 justify-center">
+        <div className="mb-2">
+          <h1 className="font-bold text-4xl">Address:</h1>
+        </div>
+        <div className="bg-white rounded-3xl px-5 shadow-xl">
+          <Input></Input>
+        </div>
+      </div>
+      <div className=" grow flex flex-col px-16 justify-center">
+        <div className="mb-2">
+          <h1 className="font-bold text-4xl">Blockchain:</h1>
+        </div>
+        <div className="bg-white rounded-3xl px-5 shadow-xl">
+          <Input
+            onClick={() => setVisible(true)}
+            value={receiver.toUpperCase()}
+            placeholder="Choose network"
+            style={{ '--text-align': 'center' , caretColor: "transparent"}}
+            className='text-4xl'
+          />
           <Picker
             columns={blockchainColumns}
-            value={blockchain}
             visible={visible}
+            value={blockchain}
             onClose={() => {
               setVisible(false);
             }}
             onConfirm={(v) => {
-              //   setBlockChain(v);
-              onChange('source', v);
+              onChange('receiver',v[0])
             }}
-          />
-        </Form.Item>
-      </Form>
+          ></Picker>
+        </div>
+      </div>
     </div>
   );
 };
--- src/app/pages/Contacts/index.tsx
+++ src/app/pages/Contacts/index.tsx
@@ -1,16 +1,19 @@
-import { Tabs, Form, Input, Button } from 'antd-mobile';
+import { Tabs, Form, Input, Button,Card } from 'antd-mobile';
 import React from 'react';
 import BackBtn from '../../component/BackBtn';
 import { useState } from 'react';
 import AddressForm from './components/AddressForm';
 import SendForm from './components/SendForm';
 import ReceiptForm from './components/ReceiptForm';
+import './styles.scss';
+import SendBtn from './components/SendBtn';
+import { SendErrorCheck } from './utils/ErrorCheck';
 
 type Props = {};
 
 export type TransactionDetail = {
   address?: string;
-  amount?: number | string;
+  amount?:  string;
   token?: string;
   receiver?: string;
   source?: 'Mumbai' | 'Fuji';
@@ -25,49 +28,54 @@ const initialTransactionDetail = {
 };
 
 const Contacts: React.FC<Props> = () => {
-  const mockSend = () => {};
-  // props傳遞value下去 子組件
-  // const [address] = useState();
-  // const [amount] = useState();
-  // const [token] = useState();
-  // const [block] = useState();
   const [transactionDetail, setTransactionDetail] = useState<TransactionDetail>(initialTransactionDetail);
+  const [tradingMode,setTradingMode] = useState(true); // 为了实现添加联系人时，控制send 按钮不渲染。
 
   const handleTransactionDetail = (key: keyof TransactionDetail, value: any) => {
     setTransactionDetail((prev) => ({ ...prev, [key]: value })); // 把变量名为 key 的值设为 value
   };
 
+  const handleTransfer = () => {
+    try {
+      // error check
+      const errorMessage = SendErrorCheck(transactionDetail)
+      if(errorMessage !== null) {
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   return (
-    <div className="h-80">
+    <div >
       <BackBtn />
-      <main className="grid grid-flow-row gap-4">
+      <main className="flex flex-col pt-0">
         {/* Send Form*/}
-        <div className="mx-8">
-          <h1 className="" style={{ color: '#0A3D53' }}>
-            Send
-          </h1>
-          <SendForm />
+        <div className="flex-auto p-8">
+          <h1 className="text-5xl" style={{ color: '#0A3D53' }}>
+                Send
+              </h1>
+            <SendForm {...transactionDetail} onChange={handleTransactionDetail}/>
         </div>
         {/* To Form*/}
-        <div>
-          <h1 className="mx-8" style={{ color: '#0A3D53' }}>
-            To
-          </h1>
-          <Tabs style={{ background: '#D9D9D9B2', margin: 'none' }}>
-            <Tabs.Tab title="Address" key="add">
+        <div className='flex-auto h-16 px-8 py-4'>
+            <h1 className="text-5xl" style={{ color: '#0A3D53' }}>
+              To
+            </h1>
+        </div>
+        <div className='flex-auto p-1'>
+          <Tabs className='p-1' style={{ background: 'rgba(217, 217, 217, 0.7)', margin: 'none' }}>
+            <Tabs.Tab title="Address" key="add" className='h-28 flex items-center font-bold text-2xl'>
               <AddressForm {...transactionDetail} onChange={handleTransactionDetail} />
             </Tabs.Tab>
-            <Tabs.Tab title="Receipt" key="rec">
-              <ReceiptForm {...transactionDetail} onChange={handleTransactionDetail} />
+            <Tabs.Tab title="Receipt" key="rec" className='h-28 flex items-center'>
+              <ReceiptForm {...transactionDetail} onChange={handleTransactionDetail} setTradingMode={setTradingMode} />
             </Tabs.Tab>
           </Tabs>
         </div>
         {/* Send Btn*/}
-        <div>
-          <Button block size="large" className="rounded-2xl" onClick={() => mockSend()}>
-            {' '}
-            Send <i>&gt;</i>{' '}
-          </Button>
+        <div className='flex-auto'>
+          {  tradingMode ? <SendBtn /> : null }
         </div>
       </main>
     </div>
