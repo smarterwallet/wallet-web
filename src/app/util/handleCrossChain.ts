048853a6d23f8d3dae7290b5bcef36aa6515675d
gray_jiang <Gray_Jy@yeah.net>
2023-12-06 23:38:37 +0800

    ✨ feat(config): add fuji

--- src/app/util/handleCrossChain.ts
+++ src/app/util/handleCrossChain.ts
@@ -4,7 +4,10 @@ import liquidityPoolAbi from '../../abis/liquidityPool.abi.json';
 import sourceChainSenderAbi from '../../abis/sourceChainSender.abi.json';
 import usdcAbi from '../../abis/usdc.abi.json';
 import { Global } from '../../server/Global';
+import { Config } from '../../server/config/Config';
+import { assert } from 'console';
 
+// fuji -----> mumbai
 // Polygon_Mumbai
 const MUMBAI_USDC = '0x9999f7fea5938fd3b1e26a12c3f2fb024e194f97';
 const MUMBAI_LIQUIDITY_POOL = '0x1510c7Bd9c9c7D24B0a1c54CdEb062213A5afB79';
@@ -18,10 +21,11 @@ const Fuji_DEST_CHAIN_RECEIVER = '0x4Ad8C9b33a5dDd7A4762948153Ebd43Bcf8E91Ad';
 const Fuji_SOURCE_CHAIN_SENDER = '0x4faE92E949Ed605b9ac9E7ee1cdCA164CF54410E';
 
 const FUND_AMOUNT = ethers.utils.parseUnits('1', 6);
-const destinationChainSelector = '12532609583862916517'; // Polygon Mumbai
+const mumbai_destinationChainSelector = '12532609583862916517'; // Polygon Mumbai
+const fuji_destinationChainSelector = '14767482510784806043'; // Avax Fuji
 const feeToken = '1';
 const to = '0xeb23E1a2784931A65D671DaA1235c8ae6435A367';
-const receiver = '0x4De984c203109756eb6365a696037E599dCd973C';
+const receiver = '0x4De984c203109756eb6365a696037E599dCd973C'; // MUMBAI_DEST_CHAIN_RECEIVER
 const amount = '1000000';
 
 type TransactionDetail = {
@@ -35,29 +39,42 @@ type TransactionDetail = {
 };
 
 export const handleCrossChain = async () => {
-  const deployer = new ethers.Wallet(
-    'ff3a619d9054ec33cd3f6934a6d9435accd8dfbb68eee507fa042dfb3b807635',
-    Global.account.ethersProvider,
-  );
   const signer = new ethers.Wallet(localStorage.getItem('pk'), Global.account.ethersProvider);
-  const sourceChainSender = new ethers.Contract(Fuji_SOURCE_CHAIN_SENDER, sourceChainSenderAbi, deployer);
+  const sourceChainSender = new ethers.Contract(Fuji_SOURCE_CHAIN_SENDER, sourceChainSenderAbi);
   const usdc = new ethers.Contract(Fuji_USDC, usdcAbi, signer);
-  const approveTx = await usdc.approve(sourceChainSender.address, FUND_AMOUNT, { gasLimit: 5000000 });
-  await approveTx.wait(1);
+  console.log(Global.account);
+
+  const gasPrice = await Global.account.getGasPrice();
+  const approveTx = await Global.account.sendTxApproveERC20Token(
+    usdc.address,
+    sourceChainSender.address,
+    FUND_AMOUNT,
+    Config.ADDRESS_TOKEN_PAYMASTER,
+    Config.ADDRESS_ENTRYPOINT,
+    gasPrice,
+  );
+  //   const approveTx = await usdc.approve(sourceChainSender.address, FUND_AMOUNT, { gasLimit: 5000000 });
   console.log('fund 1 USDC to SourceChainSender Contract...');
   const fundTx = await sourceChainSender.fund(FUND_AMOUNT, {
     gasLimit: 5000000,
   });
   await fundTx.wait(1);
   console.log('transaction successfully...');
-  const balance = await usdc.balanceOf(signer.address);
+  const balance = await Global.account.getBalanceOfERC20(usdc.address, Global.account.contractWalletAddress, 6);
   console.log(`Deployer has balance: ${ethers.utils.formatEther(balance)} USDC`);
 
   // Call the sendMessage function to cross-chain 1 USDC to Mumbai
   console.log('Call sendMessage function...');
-  const crossChainTx = await sourceChainSender.sendMessage(destinationChainSelector, receiver, feeToken, to, amount, {
-    gasLimit: 5000000,
-  });
+  const crossChainTx = await sourceChainSender.sendMessage(
+    mumbai_destinationChainSelector,
+    receiver,
+    feeToken,
+    to,
+    amount,
+    {
+      gasLimit: 5000000,
+    },
+  );
   console.log(`Cross-chain transaction hash: ${crossChainTx.hash}`); // copy it to Chainlink CCIP Explorer page, check cross chain status.
   await crossChainTx.wait(1);
   console.log('Cross Chain 1 USDC from Avalanche Fuji to Polygon Mumbai is successfully!');
