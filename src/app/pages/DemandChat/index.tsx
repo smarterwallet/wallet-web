775a4630236552ecd7d3635b1d7edbb336ddff54
cjchen5210 <cwj5210@gmail.com>
2023-12-10 08:43:30 +0000

    feat: cross chain tx

--- src/app/pages/DemandChat/index.tsx
+++ src/app/pages/DemandChat/index.tsx
@@ -5,7 +5,9 @@ import { useState, useRef, useEffect } from 'react';
 import MessageItem from './MessageItem';
 import { Global } from '../../../server/Global';
 import { crossChainAbstractionDemand } from '../../../server/utils/ChainlinkTx';
-import mumbai from "../../config/mumbai.json"
+import mumbai from '../../config/mumbai.json';
+import { TransactionDetail } from '../../types';
+import Cross from '../Cross';
 
 interface Conversations {
   content: string;
@@ -13,12 +15,12 @@ interface Conversations {
   isResponse: boolean;
 }
 interface Ops {
-  type: string
-  source_chain: string
-  token: string
-  amount: string
-  receiver: string
-  target_chain: string
+  type: string;
+  source_chain: string;
+  token: string;
+  amount: string;
+  receiver: string;
+  target_chain: string;
 }
 interface Result {
   category: string;
@@ -28,43 +30,62 @@ interface Result {
   };
 }
 
-
 const DemandChat = () => {
   const [inputDemand, setInputDemand] = useState<string>('');
   const [conversation, setConversation] = useState<Conversations[]>([
     { content: 'What would you need?', displayButton: false, isResponse: true },
   ]);
-  const [balanceMumbai, setBalanceMumbai] = useState<string>("")
+  const [balanceMumbai, setBalanceMumbai] = useState<string>('');
   const chatContentRef = useRef(null);
-  const [ops, setOps] = useState<Ops>()
+  const [ops, setOps] = useState<Ops>();
+  const [isCross, setIsCross] = useState(false);
+  const [transactionDetail, setTransactionDetail] = useState<TransactionDetail>(null);
 
   const handleInputOnKeyDown = async (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (inputDemand === "") return
+    if (inputDemand === '') return;
     if (event.key === 'Enter') {
       const question = { content: inputDemand, displayButton: false, isResponse: false };
       const userDemand = `Current Mumbai balance: ${balanceMumbai}USDC, Fuji balance: 0USDC. ${inputDemand}`;
       setConversation((pre) => [...pre, question]);
-      setInputDemand("")
-      const result: Result = await crossChainAbstractionDemand(userDemand)
-      console.log(`demand response is ${result.detail.reply}`)
+      setInputDemand('');
+      const result: Result = await crossChainAbstractionDemand(userDemand);
+      console.log(`demand response is ${result.detail.reply}`);
       if (result.detail.ops == null || result.detail.ops.length > 1) {
-        setConversation((pre) => [...pre, { content: result.detail.reply, displayButton: false, isResponse: true }])
+        setConversation((pre) => [...pre, { content: result.detail.reply, displayButton: false, isResponse: true }]);
       } else if (result.detail.ops) {
         // sendTxTransferERC20Token
-        setConversation((pre) => [...pre, { content: result.detail.reply, displayButton: true, isResponse: true }])
-        setOps(result.detail.ops[0])
+        setConversation((pre) => [...pre, { content: result.detail.reply, displayButton: true, isResponse: true }]);
+        setOps(result.detail.ops[0]);
       }
-
     }
   };
 
   const handleConfirmTx = async () => {
-    console.log("test.....")
-    if (ops.type === "chain-internal-transfer") {
-      const gasFee = await Global.account.getGasPrice()
-      Global.account.sendTxTransferERC20Token("0x9999f7Fea5938fD3b1E26A12c3f2fb024e194f97", ops.amount, ops.receiver, mumbai.address.address_token_paymaster, mumbai.address.address_entrypoint, gasFee)
+    if (ops.type === 'chain-internal-transfer') {
+      const gasFee = await Global.account.getGasPrice();
+      Global.account.sendTxTransferERC20TokenWithUSDCPay(
+        '0x9999f7Fea5938fD3b1E26A12c3f2fb024e194f97',
+        ops.amount,
+        ops.receiver,
+        mumbai.address.address_token_paymaster,
+        mumbai.address.address_entrypoint,
+        gasFee,
+      );
     }
-  }
+    if (ops.type === 'cross-chain-transfer') {      
+      const gasFee = await Global.account.getGasPrice();
+      const txDetail: TransactionDetail = {
+        receiver: ops.receiver,
+        amount: ops.amount,
+        source: 'mumbai',
+        target: 'fuji',
+        token: 'USDC',
+        fees: gasFee.toString(),
+      }
+      setTransactionDetail(() => txDetail)
+      setIsCross(true);
+    }
+  };
 
   useEffect(() => {
     if (chatContentRef.current) {
@@ -73,33 +94,54 @@ const DemandChat = () => {
   }, [inputDemand, conversation]);
   useEffect(() => {
     const getBalance = async () => {
-      const balance = await Global.account.getBalanceOfERC20("0x9999f7Fea5938fD3b1E26A12c3f2fb024e194f97", address, 6)
-      setBalanceMumbai(() => balance)
-      console.log(`mumbai balance is ${balance}`)    
-    }
+      const balance = await Global.account.getBalanceOfERC20('0x9999f7Fea5938fD3b1E26A12c3f2fb024e194f97', address, 6);
+      setBalanceMumbai(() => balance);
+      console.log(`mumbai balance is ${balance}`);
+    };
     const address = Global.account.contractWalletAddress;
-    getBalance()
-    console.log(`address is ${address}`)
-  }, [])
+    getBalance();
+    console.log(`address is ${address}`);
+  }, []);
   return (
     <div className="ww-page-container page-demand">
       <HeaderBar text="Demand" returnable={false} />
-      <div className="chat-content" ref={chatContentRef}>
-        {conversation.map((item, index) => (
-          <MessageItem key={index} content={item.content} displayButton={item.displayButton} isResponse={item.isResponse} handleConfirmTx={handleConfirmTx} />
-        ))}
-      </div>
-      <div className="other-demand">
-        <div className="txt">
-          <Input
-            type="text"
-            placeholder="What else do you want?"
-            value={inputDemand}
-            onChange={(e) => setInputDemand(e.target.value)}
-            onKeyDown={handleInputOnKeyDown}
+      {!isCross ? (
+        <div>
+          <div className="chat-content" ref={chatContentRef}>
+            {conversation.map((item, index) => (
+              <MessageItem
+                key={index}
+                content={item.content}
+                displayButton={item.displayButton}
+                isResponse={item.isResponse}
+                handleConfirmTx={handleConfirmTx}
+              />
+            ))}
+          </div>
+          <div className="other-demand">
+            <div className="txt">
+              <Input
+                type="text"
+                placeholder="What else do you want?"
+                value={inputDemand}
+                onChange={(e) => setInputDemand(e.target.value)}
+                onKeyDown={handleInputOnKeyDown}
+              />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <Cross
+            receiver={transactionDetail.receiver}
+            amount={transactionDetail.amount}
+            source={transactionDetail.source}
+            target={transactionDetail.target}
+            token={transactionDetail.token}
+            fees={transactionDetail.fees}
           />
         </div>
-      </div>
+      )}
     </div>
   );
 };
