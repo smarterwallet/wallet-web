8557933196b657f8a71323930712242337cde543
Ruiz000 <943461216@qq.com>
2023-12-14 17:19:39 +0800

    🐞 fix(src/app/pages/Contacts/utils/ErrorCheck.ts): 修复输入过大数值时，余额比较不通过不报错
    需要对number类型进行toString()再parseFloat(),否则有可能导致NaN的出现，隐型bug。

--- src/app/pages/Contacts/utils/ErrorCheck.ts
+++ src/app/pages/Contacts/utils/ErrorCheck.ts
@@ -1,19 +1,16 @@
 import {BalanceData, TransactionDetail} from '../index';
 
 export const SendErrorCheck = (transactionDetail : TransactionDetail, { balance } : BalanceData) => {
-    if (transactionDetail.address === '' || transactionDetail.address === null || transactionDetail.address === undefined) {
+      if (transactionDetail.address === '' || transactionDetail.address === null || transactionDetail.address === undefined) {
         return "Sender can't not be empty"
       }
-      if (transactionDetail.address.lastIndexOf('0x') != 0) {
+      if (transactionDetail.address.lastIndexOf('0x') !== 0) {
         return 'This is no a ETH WALLET address syntax'
       }
       if (transactionDetail.address.length < 42) {
         return "The length of Sender's address is not a ETH WALLET address"
       }
-      if (parseFloat(transactionDetail.amount as string) <  0) {
-        return "The amount can't be neviage"
-      }
-      if (parseFloat(transactionDetail.amount as string) > (balance['fuji'] + balance['mumbai'])) {
+      if (parseFloat(transactionDetail.amount.toString()) > (parseFloat(balance['fuji'].toString()) + parseFloat(balance['mumbai'].toString()))) {
         return "Your Wallet Balance is not enough"
       }
       if (transactionDetail.receiver === '' || transactionDetail.receiver === null || transactionDetail.receiver === undefined) {
@@ -30,3 +27,18 @@ export const SendErrorCheck = (transactionDetail : TransactionDetail, { balance 
       }
       return null
 }
+
+// const epsilon = 0.00000001; // 设置一个较小的误差范围
+//       const amount = parseFloat(transactionDetail.amount.toString());
+//       const fujiBalance = balance['fuji'];
+//       const mumbaiBalance = balance['mumbai'];
+
+//       const totalBalance = parseFloat(fujiBalance.toString()) + parseFloat(mumbaiBalance.toString());
+//       if(!isNaN(amount) && !isNaN(totalBalance)) {
+//         const isLarge = Math.abs(amount - totalBalance) > epsilon;
+
+//         console.log(isLarge);
+//       } else {
+//         console.log(isNaN(amount));
+//         console.log(isNaN(totalBalance));
+//       }
