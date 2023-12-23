ce8ad2f8a3df6a8f8cd76ca2c6abcb21fa457afa
Ruiz000 <943461216@qq.com>
2023-12-23 22:03:46 +0800

    ✨ feat(src/app/pages/Contacts/components/BlockchainPicker /ReceiptForm): 增大picker字体,联系人改为选择器

--- src/app/pages/Contacts/components/BlockchainPicker/index.tsx
+++ src/app/pages/Contacts/components/BlockchainPicker/index.tsx
@@ -2,6 +2,7 @@ import { Picker } from 'antd-mobile';
 import { blockchainColumns } from '../../utils/blockchainConfig';
 import { PickerValue } from 'antd-mobile/es/components/picker-view';
 import { TransactionDetail } from '../..';
+import './styles.scss';
 
 type Pickerparameter = {
     visible: boolean,
