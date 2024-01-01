fbe82f2dc8562b283d22f337598997cbfa1f40e6
onlyheartt8 <422474821@qq.com>
2024-01-02 03:35:58 +0800

    🌈 style:
    MessageItem 换行异常样式修复

--- src/app/pages/DemandChat/MessageItem.tsx
+++ src/app/pages/DemandChat/MessageItem.tsx
@@ -1,22 +1,24 @@
-import { message } from "antd";
+import { message } from 'antd';
 
 interface MessageItemProps {
   content: string;
   displayButton: boolean;
   isResponse: boolean;
-  handleConfirmTx: () => void
+  handleConfirmTx: () => void;
 }
 
 const MessageItem: React.FC<MessageItemProps> = ({ content, displayButton, isResponse, handleConfirmTx }) => {
-
   return (
     <div>
       {isResponse ? (
         <div className="flex flex-col mb-3">
           <div className="flex-1 w-[90%]">
-            <div className="inline-block rounded-lg">
+            <div className="max-w-full inline-block rounded-lg">
               <div
-                className={`w-full bg-[#13b6ea4d] whitespace-normal m-0 p-4  text-[40px] ${
+                style={{
+                  wordWrap: 'break-word',
+                }}
+                className={`w-full break-normal bg-[#13b6ea4d] whitespace-normal m-0 p-4  text-[40px] ${
                   displayButton ? 'rounded-t-lg' : 'rounded-lg'
                 }`}
               >
@@ -25,7 +27,10 @@ const MessageItem: React.FC<MessageItemProps> = ({ content, displayButton, isRes
               {displayButton && (
                 <div className="w-auto flex m-0">
                   <button className="flex-1 bg-white rounded-none rounded-bl-lg border-r-2 border-black">Change</button>
-                  <button className="flex-1 bg-white rounded-none rounded-br-lg border-l-2 border-black" onClick={handleConfirmTx}>
+                  <button
+                    className="flex-1 bg-white rounded-none rounded-br-lg border-l-2 border-black"
+                    onClick={handleConfirmTx}
+                  >
                     Confirm
                   </button>
                 </div>
@@ -35,10 +40,17 @@ const MessageItem: React.FC<MessageItemProps> = ({ content, displayButton, isRes
           <div className="flex-1 w-[10%]"></div>
         </div>
       ) : (
-        <div className="flex flex-col mb-3 ml-2">          
+        <div className="flex flex-col mb-3 ml-2">
           <div className="flex-1">
             <div className="flex justify-end">
-              <div className="inline-block bg-white p-4 whitespace-normal rounded-lg text-[#053346] text-[40px]">{content}</div>
+              <div
+                style={{
+                  wordWrap: 'break-word',
+                }}
+                className="max-w-full inline-block bg-white p-4 whitespace-normal rounded-lg text-[#053346] text-[40px]"
+              >
+                {content}
+              </div>
             </div>
           </div>
         </div>
